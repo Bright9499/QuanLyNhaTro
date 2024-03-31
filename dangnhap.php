1a0d95eedd189340894a650ffda51e2803e71cb2
MinhNgoc <minhngoc9499@gmail.com>
2024-03-31 22:44:12 +0700

    first commit

---
+++ dangnhap.php
@@ -0,0 +1,91 @@
+<?php
+include 'database.php';
+if(isset($_POST['dangnhap'])){
+    $email=$_POST['email'];
+    $email1=$_POST['email1'];
+    $sql= "SELECT * FROM user WHERE email='$email' AND pass='$email1'";
+    $result = mysqli_query($conn,$sql);
+    $user = mysqli_fetch_assoc($result);
+    if($result && mysqli_num_rows($result) > 0) {
+        // Successful login
+        header("Location: index.php?id=" .$user["ID"] . "");
+        exit(); // Stop script execution after redirection
+    } else {
+        // Unsuccessful login
+        echo "Không thành công";
+        die(); // Stop script execution
+    }
+}
+?>
+<?php
+    if(isset($_GET['success'])) {
+      echo " <p></p>";
+        echo "<p>Đăng ký thành công!</p>";
+    } elseif(isset($_GET['error'])) {
+        echo "<p>{$_GET['error']}</p>";
+    }
+    ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/dangnhap.css'>
+</head>
+<html>
+<body>
+    <div class="container">
+        <div class="left-section">
+          <img src="img/awkward-living-room-layout-ideas-768x461.jpg">
+        </div>
+        <div class="box-login">
+          <div class="box-header">
+            <h3>Sign in</h3>
+          </div>
+          <div class="box-content">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+              <div class="row">
+                <div class="input-group">
+                  <input id="login" name="email" type="text" placeholder="Login">
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-group">
+                  <input id="pass" name="email1" type="password" placeholder="Password">
+                </div>
+              </div>
+              <div class="rowlast">
+                <div class="checkbox-group">
+                  <input type="checkbox" id="remember-checkbox">
+                  <label for="remember-checkbox" class="remm">Remember me</label>
+                </div>
+                <a href="#" class="lost-pass">Forgot password?</a>
+              </div>
+              <div class="singin">
+                <button id="signin" name="dangnhap" class="sign-in">Sign in</button>
+              </div>
+              <div class="footer-column">s
+                <h4>Or join with</h4>
+                <ul class="font">
+                    <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-instagram"></i></li>
+                    <li><a href="#"><i class="fa-brands fa-tiktok"></i></a></li>                    
+              </div>
+              <div class="end">
+                <a class="text1">Not a member yet?</a>
+                <a href="dangky.php" class="lost">Sign up!</a>
+
+              </div>
+            
+            </form>
+          </div>
+            
+        </div>
+      </div>
+		<script src="js/dangnhap.js"></script>
+</body>
+</html>
+</html>
---
+++ sanphamchitiet.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Sản Phẩm Chi Tiết</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/sanphamchitiet.css'>
+</head>
+<body>
+		<header>
+		<nav>
+			<ul>
+				<li>
+					<a href="index.php">Trang chủ</a>
+				</li>
+				<li>
+					<a href="#">Về chúng tôi</a>
+				</li>
+				<li>
+					<a href="#">Thuê nhà trọ</a>
+				</li>
+				<li>
+					<a href="dangtin.php" id="btnDT">Đăng tin cho thuê</a>
+				</li>
+				<li>
+					<a href="dangnhap.php" id="btnDN">Đăng nhập</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+    <div class="container">
+        <article class="left-section">
+          <img id="myImage" class="myImage" src="img/img-20240318-201454_1710767844.jpg">
+          <div class="page-header">
+          <h1>Siêu Phẩm Gác Full Nội Thất Có Ban Công Ngay Ngã Tư Thủ </br> Đức</h1>
+       
+        <div>
+            <a class="title1"><i class="fa-solid fa-location-dot"></i> Địa chỉ: Đường Thống Nhất, Phường Bình Thọ, Thủ Đức, Hồ Chí Minh</a>
+        </div>
+        <div class="post-attributes">
+            <div class="gia"><i class="fa-solid fa-tag"></i><span>5.7 triệu/tháng</span></div>
+            <div class="rong"><span> 30m<sup>2</sup></span></div>
+           
+    </div>
+    <section class="content">
+        <div class="section-header"><h2 class="section-title">Thông tin mô tả</h2></div>
+        <div class="section-content">
+            <p>Toà Nhà Mới Xây Cực Kì Hiện Đại Cung Cấp Không Gian Sống Lý Tưởng</br>
+            Có cả TIVI - Ban Công cực chill</br> 
+            Toạ lạc: Thống Nhất, Bình Thọ, Thủ Đức</br>
+            Nằm ngay trục đường lớn, thuận lợi ra ngã tư thủ đức, KCNC, Vinhomes, và nằm giữa các </br> trường đại học
+            Đi đâu cũng tiện - Tới đâu cũng thích</br> 
+            Tiện ích xịn xò</br> 
+            Xây mới 100%, sang - xịn - mịn</br> 
+            Đầy đủ nội thất, có tivi sẵn Netflix nên cứ chill thôi</br> 
+            Ban công đón gió, đã mát còn thoáng</br> 
+            Giờ giấc tự do, không chung chủ</br> 
+            Số lượng có hạn:</br> 
+            Liên hệ ngay: 0397326338 (Zalo/Call) để được tư vấn và xem phòng</p></div>
+    </section>  
+    <section class="map">
+        <div class="section-header"><h3 class="section-title">Bản đồ</h3><address class="section-description">Địa chỉ: Đường Thống Nhất, Phường Bình Thọ, Thủ Đức, Hồ Chí Minh</address></div>
+        <div id="map" style="width:800px;height:900px;">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.5685579907026!2d106.7666016758389!3d10.844292057936691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527a7487e9bfd%3A0xd5cdae7a5e583347!2zVGjhu5FuZyBOaOG6pXQsIELDrG5oIFRo4buNLCBUaOG7pyDEkOG7qWMsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmlldG5hbQ!5e0!3m2!1sen!2s!4v1710580879853!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+
+    </section>  
+</div>
+        </article>
+    <div class="box-left">
+        <article class="left-section1">
+            <div class="box-header">
+              <div class="author-aside">
+                <figure class="author-avatar">
+                  <img src="https://hinhnen123.com/wp-content/uploads/2021/06/anh-avatar-dep-nhat-11.jpg" alt="Author Avatar">
+                </figure>
+                <h4 class="author-name">Trần Văn Long</h4>
+				  <a class="xemTro" href="xemTro.php">Nhấn vào đây để đặt lịch xem trọ</a>
+              </div>
+              <table class="author-contact">
+                <tr>
+                  <td class="contact-label"><i class="fa-solid fa-phone"></i></td>
+                  <td class="contact-info">0397326338</td>
+                </tr>
+                <tr>
+                  <td class="contact-label"><i class="fa-solid fa-envelope"></i></td>
+                  <td class="contact-info">longtran@gmail.com</td>
+                </tr>
+                <tr>
+                    <td class="contact-label"><i class="fa-brands fa-facebook"></i></td>
+                    <td class="contact-info">Trần Văn Long</td>
+                  </tr>
+              </table>
+            </div>
+          </article>
+          <div class="box-right">
+            <h3 class="highlighted-news-title">Tin nổi bật</h3>
+            <ul class="highlighted-news-list">
+              <li class="highlighted-news-item">
+                <img src="img/1hp8ogn60-83923t_1710769215.jpg" alt="Author Avatar">
+                <a class="title2"> Studio full nội thất Q1 ngay mặt tiền đường </a>
+              </li>
+              <li class="highlighted-news-item">
+                <img src="img/z2095121778780-339d6834a1ac779bfc0aed8e2bb4e66b_1691035301.jpg" alt="Author Avatar">
+                <a class="title2">Cho thuê phòng trọ tại Phường 3, Quận Gò Vấp</a>
+              </li>
+              <li class="highlighted-news-item">
+                <img src="img/img-20240318-201454_1710767844.jpg" alt="Author Avatar">
+                <a class="title2">Phòng thoáng mát, mới xây ngay Phan Xích Long Phú Nhuận</a>
+              </li>
+              <li class="highlighted-news-item">
+                <img src="img/img-20240318-201454_1710767844.jpg" alt="Author Avatar">
+                <a class="title2">Studio full nội thất, rộng rãi, ở được 3-4 người vô tư ngay trục đường phạm văn đồng</a>
+              </li>
+              <li class="highlighted-news-item">
+                <img src="img/img-20240318-201454_1710767844.jpg" alt="Author Avatar">
+                <a class="title2">Cho thuê nguyên tầng, DT 40m2, giá 5tr/tháng, đường Phan Đăng Lưu</a>
+              </li>
+
+            </ul>
+          </div>
+</div>
+</div>
+<footer class="footer">
+		<p>&copy; 2024 Nhà trọ FPOLY. Bảo lưu mọi quyền.</p>
+	</footer>
+        <script src="js/sanphamchitiet.js">
+        </script>
+</body>
+</html>
